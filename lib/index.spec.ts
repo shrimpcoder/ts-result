a4d3be25722bb8deb9a647bccd3991e17455ba62
shrimpcoder <shrimpcoder97@gmail.com>
2024-09-15 23:31:09 +0900

    Initial commit

---
+++ lib/index.spec.ts
@@ -0,0 +1,7 @@
+import { sum } from './index';
+
+describe('index', () => {
+  it('should work', () => {
+    expect(sum(1, 1)).toBe(2);
+  });
+});
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        main: './lib/index.ts',
+      },
+      formats: ['es', 'cjs'],
+      name: 'library-template',
+      fileName: (format) => {
+        return `${format}/index.js`;
+      },
+    },
+  },
+  test: {
+    globals: true,
+    include: ['lib/**/*.test.ts', 'lib/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+    },
+  },
+});
